3c51882719cf59fbd24a4cbac396e986ce52f0a3
saxenabhishek <as7122000@gmail.com>
2020-07-29 11:04:19 +0530

    CSRF token implementation

--- Crawler/Crawler/settings.py
+++ Crawler/Crawler/settings.py
@@ -14,7 +14,8 @@ NEWSPIDER_MODULE = 'Crawler.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'Crawler (+http://www.yourdomain.com)'
+#USER_AGENT = 'Cupcake (+http://www.ra.tech)'
+#USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
--- Crawler/Crawler/spiders/Linkden.py
+++ Crawler/Crawler/spiders/Linkden.py
@@ -9,9 +9,29 @@ class LoginSpider(scrapy.Spider):
     def parse(self,response):
         self.logger.info("HI pls work")
         csrf_token = response.css("input::attr('value')").getall()
-        print("csrf",csrf_token)
-        yield csrf_token
-        #yield FormRequests.from_response(response, formdata={'csrf_token': csrf_token, 'user':'user', 'pass','pass'}, callback=self.parse_after_login)
+        print(csrf_token)
+        formdata = {
+            "csrfToken":csrf_token[0],
+            "session_key":"as7122000@gmail.com",
+            "ac":csrf_token[1],
+            "sIdString":csrf_token[2],
+            "parentPageKey":csrf_token[3],
+            "pageInstance":csrf_token[4],
+            "trk": "guest_homepage-basic_nav-header-signin",
+            "authUUID":"",
+            "session_redirect":"",
+            "loginCsrfParam":csrf_token[8],
+            "fp_data":csrf_token[9],
+            "apfc":csrf_token[10],
+            "_d":csrf_token[11],
+            "showGoogleOneTapLogin":csrf_token[12],
+            "controlId":csrf_token[13],
+            "session_password":"!Abhi987",
+            "loginFlow":"REMEMBER_ME_OPTIN"
+        }
+        print(formdata)
+        FormRequest.from_response(response, formdata=formdata, callback=self.parse)
+        yield FormRequest.from_response(response, formdata=formdata, callback=self.parse)
 
     def parse_after_login(self,response):
-        pass
+        print("Inside this now")
